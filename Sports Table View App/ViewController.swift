5f50959c4eb2318e2ca916e63715e2cadfbf9c4b
Anour <emprors2@gmail.com>
2020-02-27 14:08:08 -0500

    Initial Commit

---
+++ Sports Table View App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sports Table View App
+//
+//  Created by Bouna Anour NDIAYE on 2020-02-27.
+//  Copyright © 2020 Bouna Anour NDIAYE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
